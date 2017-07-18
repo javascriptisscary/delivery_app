10727386eece51967b2e2c94ef2e1e1ce9ffe93f
Kenneth <crazedpokerpro@yahoo.com>
2017-07-17 22:59:50 -0500

    add 400 errors to meal api

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -2,16 +2,25 @@ class Api::V1::MealsController < ActionController::API
   include DateHelper
  
   def index
-    @date = get_date_from_current_week(params[:day]) # Because we are using dates in db, we must match "day" with date in the current week
-    @zone = DeliveryZone.find(params[:zone_id])
-    @meals = @zone.meals.where(delivery_date: @date)
-     
-    render json: @meals, each_serializer: Api::V1::MealSerializer
+    if params[:day] && params[:zone_id]
+      @date = get_date_from_current_week(params[:day]) # Because we are using dates in db, we must match "day" with date in the current week
+      @zone = DeliveryZone.find(params[:zone_id]) # get zone
+      @meals = @zone.meals.where(delivery_date: @date) #get meals from zone with supplied day(date)
+      
+      render json: @meals, each_serializer: Api::V1::MealSerializer
+    else
+      render json: {status: 400, message: "Can't find meals without zone_id and day"}, status: 400
+    end
   end
   
   def show
-    @meal = Meal.find(params[:id])
-    render json: @meal, serializer: Api::V1::MealSerializer
+    if params[:id]
+      @meal = Meal.find(params[:id])
+      
+      render json: @meal, serializer: Api::V1::MealSerializer
+    else
+      render json: {status: 400, message: "Can't find meal without id"}, status: 400
+    end
   end
   
   private
--- spec/requests/api/v1/meals_spec.rb
+++ spec/requests/api/v1/meals_spec.rb
@@ -1,38 +1,50 @@
 require 'rails_helper'
 
 describe "Meals API" do
-  it '#SHOW' do
+  context '#SHOW' do
     # {"id"=>1, "name"=>"Lamb Soup", "restaurant"=>{"id"=>2, "name"=>"Example Restaurant",
     # "created_at"=>"2017-07-16T20:54:16.733Z", "updated_at"=>"2017-07-16T20:54:16.733Z"}}
-    
-    @meal = create(:meal, name: "Lamb Soup", id: 1)
-
-    get '/api/v1/meals/1'
-    
-    expect(response).to be_success
-    expect(response.content_type).to eq("application/json") 
-    expect(response_body_as_json['id']).to eql(@meal.id)
-    expect(response_body_as_json['name']).to eql(@meal.name)
-    expect(response_body_as_json.dig('restaurant','name')).to eql(@meal.restaurant.name)
+    it "returns correct json meal" do
+      @meal = create(:meal, name: "Lamb Soup", id: 1)
+  
+      get '/api/v1/meals/1'
+      
+      expect(response).to be_success
+      expect(response.content_type).to eq("application/json") 
+      expect(response_body_as_json['id']).to eql(@meal.id)
+      expect(response_body_as_json['name']).to eql(@meal.name)
+      expect(response_body_as_json.dig('restaurant','name')).to eql(@meal.restaurant.name)
+    end
   end
     
-  it '#INDEX' do
+  context '#INDEX' do
     # {"id"=>1, "name"=>"Tom Yum", "restaurant"=>{"id"=>2, "name"=>"Restaurant 2", "delivery_zone_id"=>2, "created_at"=>"2017-07-18T03:06:16.859Z", "updated_at"=>"2017-07-18T03:06:16.859Z"}}
     # {"id"=>2, "name"=>"Chicken", "restaurant"=>{"id"=>3, "name"=>"Restaurant 3", "delivery_zone_id"=>2, "created_at"=>"2017-07-18T03:06:16.861Z", "updated_at"=>"2017-07-18T03:06:16.861Z"}}
     
-    @zone = create(:delivery_zone)
-    @rest = create(:restaurant, delivery_zone: @zone)
-    @rest2 = create(:restaurant, delivery_zone: @zone)
-    @meal = create(:meal, name: "Tom Yum", restaurant: @rest, delivery_date: Date.today)
-    @meal2 = create(:meal, name: "Chicken", restaurant: @rest2, delivery_date: Date.today)
     
-    get "/api/v1/meals/?zone_id=#{@zone.id}&day=#{Date.today.strftime('%A')}"
+    it "returns correct json with meals" do
+      @zone = create(:delivery_zone)
+      @rest = create(:restaurant, delivery_zone: @zone)
+      @rest2 = create(:restaurant, delivery_zone: @zone)
+      @meal = create(:meal, name: "Tom Yum", restaurant: @rest, delivery_date: Date.today)
+      @meal2 = create(:meal, name: "Chicken", restaurant: @rest2, delivery_date: Date.today)
+      
+      get "/api/v1/meals/?zone_id=#{@zone.id}&day=#{Date.today.strftime('%A')}"
+      
+      expect(response).to be_success
+      expect(response.content_type).to eq("application/json") 
+      expect(response_body_as_json.length).to eql(2)
+      expect(response_body_as_json[0].dig('name')).to eql(@meal.name)
+      expect(response_body_as_json[1].dig('name')).to eql(@meal2.name)
+    end
     
-    expect(response).to be_success
-    expect(response.content_type).to eq("application/json") 
-    expect(response_body_as_json.length).to eql(2)
-    expect(response_body_as_json[0].dig('name')).to eql(@meal.name)
-    expect(response_body_as_json[1].dig('name')).to eql(@meal2.name)
+    it "returns a 400 error and message" do
+      get "/api/v1/meals/?day=#{Date.today.strftime('%A')}"
+      
+      expect(response).to be_bad_request
+      expect(response.content_type).to eq("application/json")
+      expect(response_body_as_json['message']).to eq("Can't find meals without zone_id and day")
+    end
     
   end
     
