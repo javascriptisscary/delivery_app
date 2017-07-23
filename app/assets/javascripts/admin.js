89f2f456a608477af52df4f0f80dc4c5cab3b21c
Kenneth <crazedpokerpro@yahoo.com>
2017-07-23 18:40:16 -0500

    admin can create a restaurant_zone via ajax

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,4 +1,2 @@
 $("alert-danger").hide("slow", function(){ $(this).remove(); });
 $("alert-success").hide("slow", function(){ $(this).remove(); });
-
-console.log('fml');
--- app/controllers/admin/restaurant_zones_controller.rb
+++ app/controllers/admin/restaurant_zones_controller.rb
@@ -6,16 +6,22 @@ class Admin::RestaurantZonesController < ApplicationController
   end
  
   def create
+    @restaurant_zone = RestaurantZone.new(restaurant_zone_params)
+    @restaurant_zone.save
+    flash[:notice] = "New restaurant zone created with a restaurant #{@restaurant_zone.restaurant.name},
+                      zone #{@restaurant_zone.zone.name}, and date of #{@restaurant_zone.delivery_date}" 
     
+    get_restaurant_zones_for_partial(@restaurant_zone.delivery_date)
+    respond_to do |format|
+        format.js {render "by_date"}
+    end
   end
 
 
   def by_date
-    @date = params[:date]
-    @restaurant_zones = RestaurantZone.includes(:restaurant, :zone).where(delivery_date: @date)
-    @restaurants = Restaurant.all
-    @zones = Zone.all
+    @date = params[:delivery_date]
     
+    get_restaurant_zones_for_partial(@date)
     respond_to do |format|
       format.js {render layout: false}
     end
@@ -23,17 +29,24 @@ class Admin::RestaurantZonesController < ApplicationController
   
   def destroy
     @restaurant_zone = RestaurantZone.find(params[:id])
+    @date = @restaurant_zone.delivery_date
     flash[:alert] = "You have deleted restaurant zone #{@restaurant_zone.id} for date #{@restaurant_zone.delivery_date}"
     @restaurant_zone.destroy
     
-    @date = params[:date]
-    @restaurant_zones = RestaurantZone.includes(:restaurant, :zone).where(delivery_date: @date)
-    @restaurants = Restaurant.all
-    @zones = Zone.all
-    
+    get_restaurant_zones_for_partial(@date)
     respond_to do |format|
       format.js {render "by_date"}
     end
+  end
+  
+  private
+  
+  def get_restaurant_zones_for_partial(date)
+    @restaurant_zones = RestaurantZone.includes(:restaurant, :zone).where(delivery_date: date).order(:zone_id)
+  end
+  
+  def restaurant_zone_params
+    params.require(:restaurant_zone).permit(:delivery_date, :restaurant_id, :zone_id)
   end
 
 end
