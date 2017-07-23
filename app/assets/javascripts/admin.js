e1f355f4cb37779e0e2ef027036bc323883b1311
Kenneth <crazedpokerpro@yahoo.com>
2017-07-23 01:02:24 -0500

    Delete a restaurant_zone via ajax

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,4 @@
+$("alert-danger").hide("slow", function(){ $(this).remove(); });
+$("alert-success").hide("slow", function(){ $(this).remove(); });
+
+console.log('fml');
--- app/controllers/admin/restaurant_zones_controller.rb
+++ app/controllers/admin/restaurant_zones_controller.rb
@@ -4,12 +4,35 @@ class Admin::RestaurantZonesController < ApplicationController
   def index
   
   end
+ 
+  def create
+    
+  end
+
 
   def by_date
-    date = params[:date]
-    @restaurant_zones = RestaurantZone.includes(:restaurant, :zone).where(delivery_date: date)
+    @date = params[:date]
+    @restaurant_zones = RestaurantZone.includes(:restaurant, :zone).where(delivery_date: @date)
+    @restaurants = Restaurant.all
+    @zones = Zone.all
+    
     respond_to do |format|
       format.js {render layout: false}
+    end
+  end
+  
+  def destroy
+    @restaurant_zone = RestaurantZone.find(params[:id])
+    flash[:alert] = "You have deleted restaurant zone #{@restaurant_zone.id} for date #{@restaurant_zone.delivery_date}"
+    @restaurant_zone.destroy
+    
+    @date = params[:date]
+    @restaurant_zones = RestaurantZone.includes(:restaurant, :zone).where(delivery_date: @date)
+    @restaurants = Restaurant.all
+    @zones = Zone.all
+    
+    respond_to do |format|
+      format.js {render "by_date"}
     end
   end
 
